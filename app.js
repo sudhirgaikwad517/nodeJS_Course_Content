9aa9d3b6638f2d227752e965234075678bb3e874
sudhirgaikwad517 <sudhirgaikwad517@gmail.com>
2025-10-02 14:13:36 +0530

    My first node js programm

---
+++ app.js
@@ -0,0 +1,9 @@
+//simple code in js 
+
+console.log("Hello , world"); //printing the output on the console or to display the outpyt we use the cosnole.log function .
+
+function add(a,b){
+    return a+b;
+}
+
+console.log(add(2,3)); // calling the add function and printing the output on the console .
