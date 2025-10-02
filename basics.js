a2f7263e5928ef29a767dc3cabab844e5c31c7bd
sudhirgaikwad517 <sudhirgaikwad517@gmail.com>
2025-10-02 14:46:43 +0530

    Basics of Node and JS

--- basics.js
+++ basics.js
@@ -1,4 +1,49 @@
-var a = 10;
-let b =20;
-const c=30; //initializing a constant variable and its value cannot be changed later in the code.
+const data = require('./data.js');
+console.log(data.username); //printing the username property of the data module on the console.
 
+// var a = 10;
+// let b =20;
+// const c=30; //initializing a constant variable and its value cannot be changed later in the code.
+
+// // conditional statements in js
+// if(a>b){
+//     console.log("a is greater than b");
+// }
+// else{
+//     console.log("b is greater than a");
+// }
+
+// // functions in js
+
+// function multiply(x,y){
+//     return x*y;
+// }
+
+// console.log(multiply(2,3)); // calling the multiply function and printing the output on the console .
+
+// // loops in js
+// //for loop
+// for(var a=0;a<=10;a++){
+//     console.log(a);
+// }
+// //while loop
+// var a = 0;
+// while(a<=10){
+//     console.log(a);
+//     a++;
+// }
+// // array in js
+// var users=['harry','rohan','shubham'];
+
+// for(var i=0;i<users.length;i++){
+//     console.log(users[i]);
+// }
+
+// //object in js
+
+// var user ={
+//     name: 'harry',  
+//     city : 'delhi',
+//     age : 18
+// }
+// console.log(user.name); //printing the name property of the user object on the console.
