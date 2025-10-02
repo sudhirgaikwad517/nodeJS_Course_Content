9b3e4d0c353e5e3e2fb3db6216ae05874f5c6a60
sudhirgaikwad517 <sudhirgaikwad517@gmail.com>
2025-10-02 14:15:59 +0530

    Importing module in node js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //simple code in js 
 
+var fs = require('fs'); //importing the file system module in node js
+
 console.log("Hello , world"); //printing the output on the console or to display the outpyt we use the cosnole.log function .
 
 function add(a,b){
