34df6d72611d0f548f2fc8f9d0d3ce72a5fb621c
sudhirgaikwad517 <sudhirgaikwad517@gmail.com>
2025-10-02 15:25:15 +0530

    Importing the node packages or modules

---
+++ colors.js
@@ -0,0 +1,7 @@
+//importing external package
+
+const colors = require('colors'); //importing the colors package to add colors to the console output
+
+console.log("Hello , world".green); //printing the output on the console in green color using the green function of the colors package
+console.log("Hello , world".red); //printing the output on the console in red color using the red function of the colors package
+console.log("Hello , world".blue); //printing the output on the console in blue color using the blue function of the colors package
