5f358fc0bb13e8b4da3aa9b03aa04dc28076b200
sudhirgaikwad517 <sudhirgaikwad517@gmail.com>
2025-10-02 14:32:58 +0530

    variable implementation

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 //simple code in js 
 
 var fs = require('fs'); //importing the file system module in node js
+fs.writeFileSync('myfile.txt','Hello , world'); //writing the file synchronously using the writeFileSync function of the fs module
+var content = fs.readFileSync('myfile.txt','utf-8'); //reading the file synchronously using the readFileSync function of the fs module
+console.log(content); //printing the output of the writeFileSync function on the console
 
-console.log("Hello , world"); //printing the output on the console or to display the outpyt we use the cosnole.log function .
+// console.log("Hello , world"); //printing the output on the console or to display the outpyt we use the cosnole.log function .
 
 function add(a,b){
     return a+b;
---
+++ basics.js
@@ -0,0 +1,4 @@
+var a = 10;
+let b =20;
+const c=30; //initializing a constant variable and its value cannot be changed later in the code.
+
